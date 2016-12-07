a9736a4a221b0210e96d0d912288bce07075412b
Noah Berman <noah@noahberman.org>
2016-12-06 18:52:54 -0700

    config for login/logout/user create begin

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  
+	def create
+	  @response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV['GITHUB_CLIENT_KEY']}&client_secret=#{ENV['GITHUB_CLIENT_SECRET']}&code=#{params["code"]}")
+
+	  token = @response.body.split(/\W+/)[1]
+
+	  oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+
+	  auth = JSON.parse(oauth_response.body)
+
+	  user                = User.find_or_create_by(uid: auth["uid"])
+	  user.username       = auth["login"]
+	  user.uid			      = auth["id"]
+	  user.token	        = token
+	  user.save
+
+	  session[:user_id] = user.id
+
+	  redirect_to dashboard_index_path
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   
   root "pages#index"
   
+  get 'auth/github/callback', to: 'sessions#create'
+  
+  resources :dashboard, only: [:index]
+  
 end

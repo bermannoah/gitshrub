5280187995734dbc80a48b55730a97e0ff269623
Noah Berman <noah@noahberman.org>
2016-12-07 21:27:32 -0700

    image display on user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   def index
     user = User.find_by(uid: current_user.uid)
+    @avatar = user.return_avatar
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  
+  def return_avatar
+    user = GithubService.new(ENV["GITHUB_USER_TOKEN"]).user
+    @avatar = user[:avatar_url]
+  end
+  
+  
+  private
+    attr_reader :avatar
+  
 end

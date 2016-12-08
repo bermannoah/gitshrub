019cedc3eb0bebc551b25e3a7d6b613afaaab96e
Noah Berman <noah@noahberman.org>
2016-12-08 13:40:55 -0700

    index page hotfix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
   
   def index
-    user = User.find_by(uid: current_user.uid)
-    @following_activity = user.return_following_activity
-    @mentions = user.return_mentions
+    if !current_user.nil?
+      user = User.find_by(uid: current_user.uid)
+      @following_activity = user.return_following_activity
+      @mentions = user.return_mentions
+    end
   end
   
 end
